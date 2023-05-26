3240e69be5f52235f68dd010c88ba877d01760a9
elenapronichkina <134228142+elenapronichkina@users.noreply.github.com>
2023-05-26 23:00:11 +0300

    Update str_func..py
    правка_опечатка

--- str_func..py
+++ str_func..py
@@ -1,6 +1,6 @@
 user_str = str(input())
 def str_func(user_str):
-    """печатает текст заглавноми буквами"""
+    """печатает текст заглавными буквами"""
     print(user_str.upper())
 
 str_func(user_str)
